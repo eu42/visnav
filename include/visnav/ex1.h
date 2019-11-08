8a79082aeded2ab07d155a2ac46862364985bcb1
Erkam Uyanik <erkam.uyanik@gmail.com>
2019-11-09 00:21:49 +0100

    implement ex1

--- include/visnav/ex1.h
+++ include/visnav/ex1.h
@@ -32,46 +32,136 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
 
+#include <cmath>
+#include <iostream>
+
 #include <sophus/se3.hpp>
 
 #include <visnav/common_types.h>
 
 namespace visnav {
 
+const Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
+
+template <class T>
+Eigen::Matrix<T, 3, 3> hat_operator(const Eigen::Matrix<T, 3, 1>& x) {
+  Eigen::Matrix<T, 3, 3> x_hat;
+  // clang-format off
+  x_hat <<
+      0, -x(2), x(1),
+      x(2), 0, -x(0),
+      -x(1), x(0), 0;
+  // clang-format on
+  return x_hat;
+}
+
+template <class T>
+std::tuple<double, Eigen::Matrix<T, 3, 1>> get_angle_and_axis(
+    const Eigen::Matrix<T, 3, 1>& x) {
+  // get angle and axis from given vector
+  double theta = x.norm();
+
+  Eigen::Matrix<T, 3, 1> a;
+  if (theta == 0) {
+    a << 1, 1, 1;
+  } else {
+    a = x / theta;
+  }
+
+  return {theta, a};
+}
+
 // Implement exp for SO(3)
 template <class T>
 Eigen::Matrix<T, 3, 3> user_implemented_expmap(
-    const Eigen::Matrix<T, 3, 1>& xi) {
-  // TODO SHEET 1: implement
-  UNUSED(xi);
-  return Eigen::Matrix<T, 3, 3>();
+    const Eigen::Matrix<T, 3, 1>& phi) {
+  auto [theta, a] = get_angle_and_axis(phi);
+  Eigen::Matrix<T, 3, 3> a_hat = hat_operator(a);
+
+  double cos_theta = cos(theta);
+  double sin_theta = sin(theta);
+
+  return cos_theta * I + (1 - cos_theta) * a * a.transpose() +
+         sin_theta * a_hat;
 }
 
 // Implement log for SO(3)
 template <class T>
 Eigen::Matrix<T, 3, 1> user_implemented_logmap(
     const Eigen::Matrix<T, 3, 3>& mat) {
-  // TODO SHEET 1: implement
-  UNUSED(mat);
-  return Eigen::Matrix<T, 3, 1>();
+  Eigen::Matrix<T, 3, 1> res = Eigen::MatrixXd::Zero(3, 1);
+
+  double theta = acos((mat.trace() - 1) / 2);
+
+  if (theta == 0) return res;
+
+  Eigen::Matrix<T, 3, 1> diff_r_rT;
+  diff_r_rT << mat(2, 1) - mat(1, 2), mat(0, 2) - mat(2, 0),
+      mat(1, 0) - mat(0, 1);
+
+  return theta * (1 / (2 * sin(theta))) * diff_r_rT;
 }
 
 // Implement exp for SE(3)
 template <class T>
 Eigen::Matrix<T, 4, 4> user_implemented_expmap(
     const Eigen::Matrix<T, 6, 1>& xi) {
-  // TODO SHEET 1: implement
-  UNUSED(xi);
-  return Eigen::Matrix<T, 4, 4>();
+  Eigen::Matrix<T, 4, 4> res = Eigen::Matrix4d::Identity();
+
+  Eigen::Matrix<T, 3, 1> ro = xi.head(3);
+  Eigen::Matrix<T, 3, 1> phi = xi.tail(3);
+  Eigen::Matrix<T, 3, 3> exp_phi = user_implemented_expmap(phi);
+
+  auto [theta, a] = get_angle_and_axis(phi);
+
+  if (theta == 0) return res;
+
+  Eigen::Matrix<T, 3, 3> a_hat = hat_operator(a);
+
+  double cos_theta = cos(theta);
+  double sin_theta_by_theta = sin(theta) / theta;
+
+  Eigen::Matrix<T, 3, 3> jacobian;
+  jacobian = sin_theta_by_theta * I +
+             (1 - sin_theta_by_theta) * a * a.transpose() +
+             (1 - cos_theta) / theta * a_hat;
+
+  res.block(0, 0, 3, 3) = exp_phi;
+  res.block(0, 3, 3, 1) = jacobian * ro;
+
+  return res;
 }
 
 // Implement log for SE(3)
 template <class T>
 Eigen::Matrix<T, 6, 1> user_implemented_logmap(
     const Eigen::Matrix<T, 4, 4>& mat) {
-  // TODO SHEET 1: implement
-  UNUSED(mat);
-  return Eigen::Matrix<T, 6, 1>();
+  Eigen::Matrix<T, 6, 1> res = Eigen::MatrixXd::Zero(6, 1);
+
+  Eigen::Matrix<T, 3, 3> r = mat.block(0, 0, 3, 3);
+  Eigen::Matrix<T, 3, 1> t = mat.block(0, 3, 3, 1);
+
+  Eigen::Matrix<T, 3, 1> phi = user_implemented_logmap(r);
+
+  auto [theta, a] = get_angle_and_axis(phi);
+
+  if (theta == 0) return res;
+
+  Eigen::Matrix<T, 3, 3> a_hat = hat_operator(a);
+
+  double cos_theta = cos(theta);
+  double sin_theta_by_theta = sin(theta) / theta;
+
+  Eigen::Matrix<T, 3, 3> jacobian;
+  jacobian = sin_theta_by_theta * I +
+             (1 - sin_theta_by_theta) * a * a.transpose() +
+             (1 - cos_theta) / theta * a_hat;
+
+  Eigen::Matrix<T, 3, 1> ro = jacobian.colPivHouseholderQr().solve(t);
+
+  res.block(0, 0, 3, 1) = ro;
+  res.block(3, 0, 3, 1) = phi;
+  return res;
 }
 
 }  // namespace visnav
