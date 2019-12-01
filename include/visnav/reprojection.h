fa3b6864bd6d901c21ac53d9976f68a0b1bb42db
Erkam Uyanik <erkam.uyanik@gmail.com>
2019-12-01 21:45:21 +0100

    ex4

--- include/visnav/reprojection.h
+++ include/visnav/reprojection.h
@@ -87,7 +87,7 @@ struct BundleAdjustmentReprojectionCostFunctor {
     const std::shared_ptr<AbstractCamera<T>> cam =
         AbstractCamera<T>::from_data(cam_model, sIntr);
 
-    // TODO SHEET 4: Compute reprojection error
+    residuals = p_2d - cam->project(T_w_c.inverse() * p_3d_w);
 
     return true;
   }
